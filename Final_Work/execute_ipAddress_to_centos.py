31c32ce720061a57fc3d2abcd4c03a3957192588
Bilge Kaan <bilgekagangurgen@msn.com>
2024-01-04 17:52:49 +0300

    file updates

--- Final_Work/execute_ipAddress_to_centos.py
+++ Final_Work/execute_ipAddress_to_centos.py
@@ -37,6 +37,9 @@ def main():
     try:
         # Mevcut IP adresini al
         current_ip = target_vm.summary.guest.ipAddress
+        # Yeni IP adresini setle
+        new_ip = "10.14.45.135"
+        new_dns = "1.1.1.1"
 
         auth = vim.vm.guest.NamePasswordAuthentication(
             username="root",
@@ -45,20 +48,19 @@ def main():
 
         # Mevcut IP adresini sil
         if current_ip:
-
             cmd_del_ip = f"sudo ip addr del {current_ip}/24 dev ens192"
             spec_del_ip = vim.vm.guest.ProcessManager.ProgramSpec(programPath="/bin/bash", arguments=f"-c '{cmd_del_ip}'")
             pid_del_ip = content.guestOperationsManager.processManager.StartProgramInGuest(target_vm, auth, spec_del_ip)
             print(f"Deleting current IP with PID {pid_del_ip}")
 
         # Yeni IP adresini setle
-        cmd_set_ip = "sudo ip addr add 10.14.45.130/24 dev ens192 && sudo ip route add default via 10.14.45.1"
+        cmd_set_ip = f"sudo ip addr add {new_ip}/24 dev ens192 && sudo ip route add default via 10.14.45.1"
         spec_set_ip = vim.vm.guest.ProcessManager.ProgramSpec(programPath="/bin/bash", arguments=f"-c '{cmd_set_ip}'")
         pid_set_ip = content.guestOperationsManager.processManager.StartProgramInGuest(target_vm, auth, spec_set_ip)
         print(f"Setting new IP with PID {pid_set_ip}")
 
         # DNS adresini setle
-        cmd2 = 'echo "nameserver 1.1.1.1" | sudo tee /etc/resolv.conf > /dev/null'
+        cmd2 = f'echo "nameserver {new_dns}" | sudo tee /etc/resolv.conf > /dev/null'
         spec2 = vim.vm.guest.ProcessManager.ProgramSpec(programPath="/bin/bash", arguments=f"-c '{cmd2}'")
         pid2 = content.guestOperationsManager.processManager.StartProgramInGuest(target_vm, auth, spec2)
         print(f"Setting DNS with PID {pid2}")
