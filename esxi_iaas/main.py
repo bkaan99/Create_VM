fec2c723eebbf9b3891647d63e4136adabde0cf3
Bilge Kaan <bilgekagangurgen@msn.com>
2024-01-08 13:47:36 +0300

    new updates

--- esxi_iaas/main.py
+++ esxi_iaas/main.py
@@ -1,5 +1,6 @@
 from esxi_iaas import copyfile
 from esxi_iaas import registry_vm
+from esxi_iaas import reconfig_vm
 
 def main():
     print("IAAS Create işlemi başlatıldı")
@@ -8,21 +9,34 @@ def main():
     copying_vm_name = "referans_centos"
 
     #burada ise cloen olacak vm icin datastore icerisinde bir klasör adı belirtiyoruz.
-    copied_folder_name = "kardes"
 
     #burada ise kayıt edilme ile oluşacak olan vm adını belirtiyoruz.
     RegisterVm_name = "esxi_centos_cumartesi"
+
+    copied_folder_name = RegisterVm_name
+
 
     #esxi host bilgileri
     esxi_host_ip = "10.14.45.11"
     esxi_user = "root"
     esxi_password = "Aa112233!"
 
+    #cpu ve ram değerleri
+    cpu_count = 4
+    memory_mb = 2048
+    disk_size_gb = 48
+
     #Dosya kopyalama işlemi
+    print("VM Dosya kopyalama işlemi başlatıldı")
     copyfile.main(copying_vm_name, copied_folder_name, esxi_host_ip, esxi_user, esxi_password)
 
     #Vm kayıt işlemi
+    print("VM kayıt işlemi başlatıldı")
     registry_vm.main(RegisterVm_name , copying_vm_name, esxi_host_ip, esxi_user, esxi_password, copied_folder_name)
+
+    #Vm reconfig işlemi
+    print("VM reconfig işlemi başlatıldı")
+    reconfig_vm.main(RegisterVm_name, esxi_host_ip, esxi_user, esxi_password, cpu_count, memory_mb, disk_size_gb)
 
     print("IAAS Create işlemi tamamlandı")
 if __name__ == "__main__":
---
+++ esxi_iaas/reconfig_vm.py
@@ -0,0 +1,62 @@
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
+import ssl
+from esxi_connection import *
+
+def reconfigure_vm(vm, cpu_count, memory_mb, disk_size_gb):
+    try:
+        # Create a VimVMConfigSpec object to specify the changes
+        spec = vim.vm.ConfigSpec()
+        spec.numCPUs = cpu_count
+        spec.memoryMB = memory_mb
+
+        # Modify the first virtual disk (assuming there is only one disk)
+        if len(vm.config.hardware.device) > 0 and isinstance(vm.config.hardware.device[0], vim.vm.device.VirtualDisk):
+            disk = vm.config.hardware.device[0]
+            disk.capacityInKB = disk_size_gb * 1024 * 1024
+
+        # Invoke ReconfigVM_Task to apply the changes
+        task = vm.ReconfigVM_Task(spec=spec)
+        WaitForTask(task)
+
+        print("VM reconfigured successfully.")
+    except Exception as e:
+        print(f"Error reconfiguring VM: {e}")
+
+def WaitForTask(task):
+    """Waits and provides updates on a vSphere task until it is completed."""
+    task_done = False
+    while not task_done:
+        if task.info.state == vim.TaskInfo.State.success:
+            task_done = True
+        elif task.info.state == vim.TaskInfo.State.error:
+            print(f"Error: {task.info.error}")
+            task_done = True
+
+def main(RegisterVm_name, esxi_host_ip, esxi_user, esxi_password, cpu_count, memory_mb, disk_size_gb):
+
+    service_instance, content = create_vsphere_connection(esxi_host_ip, esxi_user, esxi_password)
+
+    vm_name_to_reconfigure = RegisterVm_name
+
+    target_cpu_count = cpu_count
+    target_memory_mb = memory_mb
+    target_disk_size_gb = disk_size_gb
+
+    vm_to_reconfigure = get_vm_by_name(content, vm_name_to_reconfigure)
+
+    if vm_to_reconfigure is None:
+        print(f"VM {vm_name_to_reconfigure} not found.")
+        return
+
+    # Power On durumunda ise shutdown et
+    if vm_to_reconfigure.runtime.powerState == vim.VirtualMachinePowerState.poweredOn:
+        print(f"{vm_name_to_reconfigure} Powering off VM...")
+        task = vm_to_reconfigure.PowerOffVM_Task()
+        WaitForTask(task)
+
+    reconfigure_vm(vm_to_reconfigure, target_cpu_count, target_memory_mb, target_disk_size_gb)
+    Disconnect(service_instance)
+
+if __name__ == "__main__":
+    main()
