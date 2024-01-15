9cc3402c0571be8cec84a8d33685c924549d423c
Bilge Kaan <bilgekagangurgen@msn.com>
2024-01-15 15:47:54 +0300

    main.py updated

--- ESXi/IaaS/Create/main.py
+++ ESXi/IaaS/Create/main.py
@@ -1,16 +1,19 @@
 from ESXi.IaaS.Create import registry_vm
+from ESXi.IaaS.Create import copyfile
+from ESXi.IaaS.Create import reconfig_vm
+import time
 
 
 def main():
     print("IAAS Create işlemi başlatıldı")
 
     #önceden kurulmuş vm 'i kopyalayacağımız için vm adını belirtiyoruz.
-    copying_vm_name = "referans_centos"
+    copying_vm_name = "Ubuntu_Deneme"
 
     #burada ise cloen olacak vm icin datastore icerisinde bir klasör adı belirtiyoruz.
 
     #burada ise kayıt edilme ile oluşacak olan vm adını belirtiyoruz.
-    RegisterVm_name = "esxi_centos_sali"
+    RegisterVm_name = "Pzt_Ubuntu_Deneme"
 
     copied_folder_name = RegisterVm_name
 
@@ -27,7 +30,7 @@ def main():
 
     #Dosya kopyalama işlemi
     print("VM Dosya kopyalama işlemi başlatıldı")
-    #copyfile.main(copying_vm_name, copied_folder_name, esxi_host_ip, esxi_user, esxi_password)
+    copyfile.main(copying_vm_name, copied_folder_name, esxi_host_ip, esxi_user, esxi_password)
 
     #Vm kayıt işlemi
     print("VM kayıt işlemi başlatıldı")
@@ -35,7 +38,7 @@ def main():
 
     #Vm reconfig işlemi
     print("VM reconfig işlemi başlatıldı")
-    #reconfig_vm.main(RegisterVm_name, esxi_host_ip, esxi_user, esxi_password, cpu_count, memory_mb, disk_size_gb)
+    reconfig_vm.main(RegisterVm_name, esxi_host_ip, esxi_user, esxi_password, cpu_count, memory_mb, disk_size_gb)
 
     print("IAAS Create işlemi tamamlandı")
 if __name__ == "__main__":
