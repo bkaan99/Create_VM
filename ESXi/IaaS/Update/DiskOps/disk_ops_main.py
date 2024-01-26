0a3d246a16ddcf2cbbc62541711e8d9aac377705
Bilge Kaan <bilgekagangurgen@msn.com>
2024-01-26 11:23:58 +0300

    disk_ops_main.py file updated and swap control added

--- ESXi/IaaS/Update/DiskOps/disk_ops_main.py
+++ ESXi/IaaS/Update/DiskOps/disk_ops_main.py
@@ -2,6 +2,8 @@ import time
 from ESXi.IaaS.Update.DiskOps.AddDisk import add_disk_to_vm, execute_disk_to_windows, execute_disk_to_linux
 from ESXi.IaaS.Update.DiskOps.DeleteDisk import delete_disk_to_vm
 from ESXi.IaaS.Update.DiskOps.ResizeDisk import resizeDisk
+from ESXi.IaaS.Update.DiskOps.SetDisk import execute_createSWAP_disk_to_linux
+
 
 def find_os_family():
 
@@ -20,8 +22,8 @@ def main(vm_name, esxi_host_ip, esxi_user, esxi_password):
 
     disk_mod = input("1- Add_Disk\n2- Delete_Disk\n3- Resize_Disk\n")
 
+    #Add Disk
     if disk_mod == "1":
-
         add_disk_to_vm.main(vm_name_to_reconfigure=vm_name, target_disk_size_gb=target_disk_size, esxi_host_ip=esxi_host_ip, esxi_user=esxi_user, esxi_password=esxi_password)
         time.sleep(10)
 
@@ -31,11 +33,16 @@ def main(vm_name, esxi_host_ip, esxi_user, esxi_password):
         elif os_family == "linux":
             execute_disk_to_linux.main(vm_name=vm_name, esxi_host_ip=esxi_host_ip, esxi_user=esxi_user,
                                        esxi_password=esxi_password)
-
+    #Delete Disk
     elif disk_mod == "2":
         delete_disk_to_vm.main(vm_name_to_reconfigure=vm_name, esxi_host_ip=esxi_host_ip, esxi_user=esxi_user,
                                esxi_password=esxi_password)
-
+    #Resize Disk
     elif disk_mod == "3":
         resizeDisk.main(vm_name_to_reconfigure=vm_name, esxi_host_ip=esxi_host_ip, esxi_user=esxi_user,
                         esxi_password=esxi_password)
+
+    #swap alanı oluşturma istiyor musunuz?
+    swap_mod = input("1- Yes\n2- No\n")
+    if swap_mod == "1":
+        execute_createSWAP_disk_to_linux.main(vm_name, esxi_host_ip, esxi_user, esxi_password)
