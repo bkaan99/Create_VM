f6163adaf3c0b3ac040ff123bc661c110872b3cd
Bilge Kaan <bilgekagangurgen@msn.com>
2024-02-09 17:23:41 +0300

    clone_from_template.py reconfig özellikleri içerisine eklendi.

--- vCenter/IaaS/Create/clone_from_template.py
+++ vCenter/IaaS/Create/clone_from_template.py
@@ -1,16 +1,17 @@
 from pyVim.connect import Disconnect
 from vCenter.IaaS.Connections.vSphere_connection import *
 from pyVmomi import vim
+import time
 
-def clone_template(si, template_name, clone_name):
+def clone_template(si, template_name, clone_name, disk_size_gb, memory_mb, cpu_count):
     content = si.RetrieveContent()
     vm_folder = content.rootFolder.childEntity[0].vmFolder
     template = get_template_by_name(content, template_name)
 
-    clone_spec = create_clone_spec(content, template, vm_folder, clone_name)
+    clone_spec = create_clone_spec(content, template, vm_folder, clone_name, disk_size_gb, memory_mb, cpu_count)
     clone_task = template.CloneVM_Task(folder=vm_folder, name=clone_name, spec=clone_spec)
 
-    print("Cloning template. This may take a while...")
+    print(f"Cloning {template_name}. Please wait...")
     while clone_task.info.state not in [vim.TaskInfo.State.success, vim.TaskInfo.State.error]:
         continue
 
@@ -26,18 +27,35 @@ def get_template_by_name(content, template_name):
             return vm
     return None
 
-def create_clone_spec(content, template, folder, clone_name):
+def create_clone_spec(content, template, folder, clone_name, disk_size_gb, memory_mb, cpu_count):
     clone_spec = vim.vm.CloneSpec()
     relocate_spec = vim.vm.RelocateSpec()
     relocate_spec.folder = folder
-    # Here, you should provide a valid ResourcePool object
     resource_pool = get_resource_pool(content)
     relocate_spec.pool = resource_pool
     clone_spec.location = relocate_spec
-    clone_spec.powerOn = False
+    clone_spec.powerOn = True
 
     clone_spec.config = vim.vm.ConfigSpec()
     clone_spec.config.name = clone_name
+
+    # CPU ve bellek ayarlarını yapın
+    clone_spec.config.numCPUs = cpu_count
+    clone_spec.config.memoryMB = memory_mb
+
+    # Disk ayarları
+    device_changes = []
+    for device in template.config.hardware.device:
+        if isinstance(device, vim.vm.device.VirtualDisk):
+            if disk_size_gb < device.capacityInKB / 1024 ** 2:
+                disk_size_gb = device.capacityInKB / 1024 ** 2
+            disk_spec = vim.vm.device.VirtualDeviceSpec()
+            disk_spec.operation = vim.vm.device.VirtualDeviceSpec.Operation.edit
+            disk_spec.device = device
+            #disk_size_gb değeri var olan boyuttan büyük olmalı (GB)
+            disk_spec.device.capacityInKB = disk_size_gb * 1024 * 1024
+            device_changes.append(disk_spec)
+    clone_spec.config.deviceChange = device_changes
 
     return clone_spec
 
@@ -57,7 +75,7 @@ def get_cluster(content):
         break
     return cluster
 
-def main(vCenter_host_ip, vCenter_user, vCenter_password, template_name, clone_name):
+def main(vCenter_host_ip, vCenter_user, vCenter_password, template_name, clone_name, disk_size_gb, memory_mb, cpu_count):
 
     service_instance, content = create_vsphere_connection(vCenter_host_ip, vCenter_user, vCenter_password)
 
@@ -67,7 +85,19 @@ def main(vCenter_host_ip, vCenter_user, vCenter_password, template_name, clone_n
 
     try:
         # Call clone_template function with desired template name and clone name
-        clone_template(service_instance, template_name= template_name, clone_name= clone_name)
+        clone_template(service_instance,
+                       template_name= template_name,
+                       clone_name= clone_name,
+                       disk_size_gb= disk_size_gb,
+                       memory_mb= memory_mb,
+                       cpu_count= cpu_count)
+        time.sleep(2)
+        # vm poweroff ise poweron yap
+        if clone_name is not None:
+            vm = get_vm_by_name(content, clone_name)
+            if vm.runtime.powerState == vim.VirtualMachinePowerState.poweredOff:
+                print(f"{clone_name} powered off olduğu için açılıyor...")
+                task = vm.PowerOnVM_Task()
     except Exception as e:
         print("Error:", e)
 
