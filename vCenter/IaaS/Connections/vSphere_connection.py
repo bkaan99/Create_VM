d0fda11adea1447801b16ff10c9066f85bb5492b
Bilge Kaan <bilgekagangurgen@msn.com>
2024-02-13 16:01:08 +0300

    vshpere connection added exceptions

--- vCenter/IaaS/Connections/vSphere_connection.py
+++ vCenter/IaaS/Connections/vSphere_connection.py
@@ -18,9 +18,37 @@ def create_vsphere_connection(host, user, password, timeout=10):
                                                 pwd=password,
                                                 sslContext=ssl_context)
 
-    except (ssl.SSLError, socket.timeout, socket.error) as e:
-        print(f"Bağlantı Hatası: {e}")
-        # exit close the program
+
+    except ssl.SSLError as e:
+        print(f"SSL Hatası: {e}")
+        sys.exit(1)
+
+    except socket.timeout as e:
+        print(f"Soket Zaman Aşımı Hatası: {e}")
+        sys.exit(1)
+
+    except socket.error as e:
+        print(f"Soket Hatası: {e}")
+        sys.exit(1)
+
+    except vim.fault.InvalidLogin as e:
+        print(f"Geçersiz Giriş Hatası: {e}")
+        sys.exit(1)
+
+    except vim.fault.HostConnectFault as e:
+        print(f"Ana Bilgisayar Bağlantı Hatası: {e}")
+        sys.exit(1)
+
+    except vim.fault.NoPermission as e:
+        print(f"Izin Hatası: {e}")
+        sys.exit(1)
+
+    except vim.fault.NotFound as e:
+        print(f"Bulunamadı Hatası: {e}")
+        sys.exit(1)
+
+    except Exception as e:
+        print(f"Beklenmeyen bir hata oluştu: {e}")
         sys.exit(1)
 
     content = service_instance.RetrieveContent()
