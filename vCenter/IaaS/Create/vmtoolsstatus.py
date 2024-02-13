24b3eef76658abbeaff9ff98d278067ad227828f
Bilge Kaan <bilgekagangurgen@msn.com>
2024-02-13 16:09:46 +0300

    tekrarlayan metodlar kaldırıldı.

--- vCenter/IaaS/Create/vmtoolsstatus.py
+++ vCenter/IaaS/Create/vmtoolsstatus.py
@@ -1,26 +1,5 @@
-from pyVim.connect import SmartConnect, Disconnect
-from pyVmomi import vim
-import ssl
-
-def create_vsphere_connection(vCenterIP, username, password):
-    ssl_context = ssl.create_default_context()
-    ssl_context.check_hostname = False
-    ssl_context.verify_mode = ssl.CERT_NONE
-
-    service_instance = SmartConnect(host=vCenterIP,
-                                    user=username,
-                                    pwd=password,
-                                    sslContext=ssl_context)
-
-    content = service_instance.RetrieveContent()
-    return service_instance, content
-
-def get_vm_by_name(content, vm_name):
-    vm_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
-    for vm in vm_view.view:
-        if vm.name == vm_name:
-            return vm
-    return None
+from pyVim.connect import Disconnect
+from vCenter.IaaS.Connections.vSphere_connection import *
 
 def main(vCenterIP, username, password, vm_name):
     service_instance, content = create_vsphere_connection(vCenterIP, username, password)
