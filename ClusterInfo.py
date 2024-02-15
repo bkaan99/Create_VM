65b0f6c385b96c8f7461b42c2b19e5fe1fa82556
Bilge Kaan <bilgekagangurgen@msn.com>
2024-02-15 17:58:04 +0300

    added 1 stage file

---
+++ ClusterInfo.py
@@ -0,0 +1,45 @@
+import ssl
+
+from pyVim.connect import SmartConnect, Disconnect
+from pyVmomi import vim
+
+def get_vm_by_name(content, vm_name):
+    vm_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.VirtualMachine], True)
+    for vm in vm_view.view:
+        if vm.name == vm_name:
+            return vm
+    return None
+
+def get_all_resource_pools(content):
+    resource_pool_view = content.viewManager.CreateContainerView(content.rootFolder, [vim.ResourcePool], True)
+    resource_pools = resource_pool_view.view
+    resource_pool_view.Destroy()
+    return resource_pools
+
+
+def main():
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_NONE
+
+    service_instance = SmartConnect(host="10.14.45.11",
+                                    user="root",
+                                    pwd="Aa112233!",
+                                    sslContext=ssl_context)
+
+    content = service_instance.RetrieveContent()
+
+    resource_pools = get_all_resource_pools(content)
+
+    # Kaynak havuzlarını yazdır
+    print("Tüm kaynak havuzları:")
+    for resource_pool in resource_pools:
+        print(resource_pool.name)
+
+
+
+    Disconnect(service_instance)
+
+
+if __name__ == "__main__":
+    main()
