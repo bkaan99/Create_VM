f89ded77cbaf77c5bb510d389fd55f0a251a1ddc
Bilge Kaan <bilgekagangurgen@msn.com>
2024-02-20 10:49:44 +0300

    check_vm_os_family.py script added externel files folder

---
+++ vCenter/IaaS/ExternelFiles/check_vm_os_family.py
@@ -0,0 +1,36 @@
+def main(os_family, os_version):
+
+    if "Windows" in os_family:
+        if "Windows Server 2016" in os_version:
+            template_name = "bkaan_windows_template"
+            print("Windows işletim sistemi için VM Template'i seçildi.")
+            return template_name
+
+        if "Windows 2018" in os_version:
+            template_name = "bkaan_windows_template"
+            print("Windows işletim sistemi için VM Template'i seçildi.")
+            return template_name
+
+        else:
+            template_name = "bkaan_windows_template"
+            return template_name
+
+    elif "Linux" in os_family:
+        if "SUSE" in os_version:
+            template_name = "SUSE-Temp-15-3"
+            print("Linux işletim sistemi için VM Template'i seçildi.")
+            return template_name
+
+        if "Ubuntu" in os_version:
+            template_name = "Ubuntu_Deneme"
+            print("Linux işletim sistemi için VM Template'i seçildi.")
+            return template_name
+
+    elif os_family == "Other":
+        pass
+
+    elif os_family == "MacOS":
+        pass
+
+    else:
+        print("OS family not found")
