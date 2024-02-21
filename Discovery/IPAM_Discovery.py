b1090fcf86bc8d182371b46175f419d746e809f7
Bilge Kaan <bilgekagangurgen@msn.com>
2024-02-21 15:17:13 +0300

    removed blanks and changed return div_text assignment in ipam sicorver script

--- Discovery/IPAM_Discovery.py
+++ Discovery/IPAM_Discovery.py
@@ -18,6 +18,7 @@ def update_session(session):
     }
     session.headers.update(headers)
 
+
 def extract_customer_names(html_content):
     soup = BeautifulSoup(html_content, 'html.parser')
 
@@ -49,7 +50,7 @@ def extract_subnets(html_content):
 
             for row in subnet_rows:
                 if row.get('id') == 'subnetsLeft':
-                    #find div class 'subnets'
+                    # find div class 'subnets'
                     subnets = row.find_all('div', class_='subnets')
 
                     Available_subnets = subnets[0]
@@ -57,8 +58,7 @@ def extract_subnets(html_content):
 
                     Subnet_Folders_List = folder_names(Available_subnets)
 
-
-                    #Subnet_Folders_List içerisinde str değerlerin içinde sayı olan verileri kaldır
+                    # Subnet_Folders_List içerisinde str değerlerin içinde sayı olan verileri kaldır
                     Subnet_Folders_List_Names = [x for x in Subnet_Folders_List if not any(c.isdigit() for c in x)]
 
                     print(Subnet_Folders_List_Names)
@@ -66,7 +66,6 @@ def extract_subnets(html_content):
                     # for li in Available_subnets_tab1.find_all('li'):
                     #     print(li.text)
                     #
-
 
                     # for folders in Available_subnets.find_all('a'):
                     #     #folders içerisinde bulunan a etiketlerinin text'lerini al
@@ -80,26 +79,21 @@ def extract_subnets(html_content):
 
 
 def folder_names(Available_subnets):
-
     Available_subnets_tab1 = Available_subnets.find('ul')
 
     # Available_subnets_tab1 içerisindeki her bir ul etiketini tek tek sil
     for ul in Available_subnets_tab1.find_all('ul'):
         ul.decompose()
 
-
     for div in Available_subnets_tab1:
         div_text = div.text
         div_text = div_text.split('\n')
         div_text = [x for x in div_text if x]
 
+        return div_text
 
-
-
-    return div_text
 
 def main():
-
     url_customers = 'https://172.28.0.27/index.php?page=tools&section=customers'
     url_subnets = 'https://172.28.0.27/index.php?page=subnets&section=1'
 
@@ -113,7 +107,7 @@ def main():
         response = session.get(url_customers, verify=False)
         if response.status_code == 200:
 
-            #Customer Names
+            # Customer Names
             customer_names = extract_customer_names(response.text)
             print("Müşteri İsimleri:")
             for index, name in enumerate(customer_names):
@@ -124,11 +118,10 @@ def main():
     except Exception as e:
         print("Bir hata oluştu:", str(e))
 
-
     try:
         response = session.get(url_subnets, verify=False)
         if response.status_code == 200:
-            #Subnets
+            # Subnets
             print("Subnetler:")
             subnets = extract_subnets(response.text)
 
@@ -138,7 +131,6 @@ def main():
 
     except Exception as e:
         print("Bir hata oluştu:", str(e))
-
 
 
 if __name__ == "__main__":
