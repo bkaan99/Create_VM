7849a720b215a975a5434e2360713922ec0e6ea8
Bilge Kaan <bilgekagangurgen@msn.com>
2024-05-16 13:23:44 +0300

    discovery folder files update

--- Discovery/Credentials.py
+++ Discovery/Credentials.py
@@ -1,6 +1,6 @@
 
 def proxmox_credential():
-    login = {
+    proxmox_login = {
             "username": "root",
             "password": "Aa112233!",
             "realm": "pam",
@@ -9,5 +9,16 @@ def proxmox_credential():
 
     virtualizationEnvironmentIp = "10.14.46.11:8006"
 
-    return login, virtualizationEnvironmentIp
+    return proxmox_login, virtualizationEnvironmentIp
 
+def ipam_credential():
+    ipam_login = {
+        "username": "ansible",
+        "password": "Cekino123!"
+    }
+
+    ipam_api_url = "https://172.28.0.27/api/0002/"
+
+    ipam_base_url = "172.28.0.27"
+
+    return ipam_login, ipam_api_url, ipam_base_url
