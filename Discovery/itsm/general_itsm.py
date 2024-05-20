a08cc7de91ada74a83183c65adca0321ee1492b2
Bilge Kaan <bilgekagangurgen@msn.com>
2024-05-20 17:02:10 +0300

    dynamic itsm controlller update

--- Discovery/itsm/general_itsm.py
+++ Discovery/itsm/general_itsm.py
@@ -1,11 +1,15 @@
 import json
+import math
 from urllib.error import HTTPError
 from urllib.parse import urlencode
-from urllib.request import urlopen,Request
 import requests
+from Discovery import Credentials
+
 
 def get_response(endpoint='', params = None):
-    url = f"https://supporttest.glasshouse.com.tr/api/v3{endpoint}"
+    base_url, api_key = Credentials.itsm_credential()
+
+    url = f"{base_url}{endpoint}"
     api_key = "A919767F-C901-4874-B0D4-0D3EE04CD3F2"
 
     headers = {
@@ -13,22 +17,101 @@ def get_response(endpoint='', params = None):
         'Accept': 'application/json',
         'Authtoken': api_key
     }
-    response = requests.get(url, headers=headers, params=params)
+
+    if params:
+        url += "?" + urlencode({"input_data": params})
+
+    response = requests.get(url, headers=headers)
     return response
 
-def get_all_requests():
+def get_all_requests(startIndex=0, numberOfData = ''):
 
-    response = get_response('/requests')
+    if numberOfData == '':
+        numberOfData = 200
 
-    if response.status_code == 200:
-        data = response.json()
-        print("Veriler başarıyla çekildi:")
-        for request in data['requests']:
-            print(request)
+    batchSize = 100
+    print("Tüm talepler alınıyor...")
+    total_batches = math.ceil(numberOfData / batchSize)
 
-    else:
-        print(f"Veri çekme hatası: {response.status_code}")
-        print(response.json())
+    all_requests = []
+
+    for i in range(0, total_batches):
+        try:
+            input_data = {
+                "list_info": {
+                    "row_count": batchSize,
+                    "start_index": startIndex,
+                    "sort_field": "due_by_time",
+                    "sort_order": "desc",
+                    "get_total_count": True
+                }
+            }
+
+            input_data_str = json.dumps(input_data)
+
+            response = get_response('/requests', params=input_data_str)
+
+            if response.status_code == 200:
+                data = response.json()
+                all_requests.append(data['requests'])
+                for request in data['requests']:
+                    print(request)
+            else:
+                print(f"Veri çekme hatası: {response.status_code}")
+                print(response.json())
+
+            startIndex += batchSize
+
+        except HTTPError as e:
+            print(f"HTTP hatası: {e.code}")
+            print(e.read())
+            break
+
+    return all_requests
+
+def get_all_request_ids(startIndex=0):
+    numberOfData = 156612
+    batchSize = 100
+    requests_ids = []
+
+    print("Tüm talep id'leri başarıyla alındı:")
+
+    total_batches = math.ceil(numberOfData / batchSize)
+
+    for i in range(0, total_batches):
+        input_data = {
+            "list_info": {
+                "row_count": batchSize,
+                "start_index": startIndex,
+                "sort_field": "due_by_time",
+                "sort_order": "desc",
+                "get_total_count": True
+            }
+        }
+        input_data_str = json.dumps(input_data)
+
+        response = get_response('/requests', params=input_data_str)
+
+        if response.status_code == 200:
+            data = response.json()
+            for request in data['requests']:
+                print(request['id'])
+                requests_ids.append(request['id'])
+
+        else:
+            print(f"Talep id'leri alınamadı. Hata kodu: {response.status_code}")
+            print(response.json())
+
+        startIndex += batchSize
+
+        # Yüzdelik hesaplama
+        completed_percentage = ((i + 1) / total_batches) * 100
+        print(f"Tamamlanan: %{completed_percentage:.2f}")
+
+    # with open('request_ids.json', 'w') as file:
+    #     json.dump(requests_ids, file, indent=2)
+
+    return requests_ids
 
 def get_info_by_request_id(id=''):
     #get request id from get_all_requests
@@ -45,6 +128,8 @@ def get_info_by_request_id(id=''):
     else:
         print(f"Summary alınamadı. Hata kodu: {response.status_code}")
         print(response.json())
+
+    return summary
 
 def get_request_summary_by_id(id=''):
     response = get_response(f'/requests/{id}/summary')
@@ -98,11 +183,10 @@ def add_request(summary, description, requester_id, assignee_id, priority):
         print(response.json())
 
 
-
 if __name__ == "__main__":
-    get_all_requests()
-    #get_info_by_request_id('66008')
+
+    #get_all_requests()
+    #get_all_request_ids()
+    get_info_by_request_id('258497')
     #get_request_summary_by_id('258471')
     #add_request('bkaandeneme', 'Test açıklama', '1', '1', 'High')
-
-
